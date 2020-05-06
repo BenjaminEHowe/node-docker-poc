2f9ddeb2993486db7e2983513ff556f79a482c97
Benjamin Howe <ben@beh.uk>
2020-05-06 11:17:22 +0100

    Added basic application

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const PORT = 8080;
+const HOST = '::';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://[${HOST}]:${PORT}`);
